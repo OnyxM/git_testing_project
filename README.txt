27a49ec580f13606d38251363c805ee0070abf6b
onyxM <onesimemoffo@gmail.com>
2018-08-20 11:28:49 +0200

    Ajout des fichiers README.txt et indedx.php

---
+++ README.txt
@@ -0,0 +1,11 @@
+Ce projet a été créé dans le cadre de mon apprentissage de git sur openclassrooms.
+
+Je m'en vais donc de ce pas effectuer les tâches suivantes :
+
+==> Un fichier nommé "README.txt" qui présente le projet;
+
+==> Au moins 2 autres fichiers de votre choix (ils peuvent contenir des scripts de code, du texte sur un projet personnel...);
+
+==> Au moins 4 commits distincts montrant l’ajout puis la modification de certains fichiers.
+
+Et puis ce sera la fin ...
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Application du cours de git</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h2>Part 1</h2>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem cumque saepe molestiae dicta non quidem molestias sed, natus nostrum perspiciatis a aliquid error animi! Illum totam aliquid odit pariatur sed.
+			</div>
+		</div>
+		<div class="row">
+			<h2>Part 2</h2>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem cumque saepe molestiae dicta non quidem molestias sed, natus nostrum perspiciatis a aliquid error animi! Illum totam aliquid odit pariatur sed.
+			</div>
+		</div>
+		<div class="row">
+			<h2>Part 3</h2>
+			<div>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem cumque saepe molestiae dicta non quidem molestias sed, natus nostrum perspiciatis a aliquid error animi! Illum totam aliquid odit pariatur sed.
+			</div>
+		</div>
+	</div>
+</body>
+</html>
